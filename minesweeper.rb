3dec56fb32c4078bb11f0d1da6042fcd1475d5c3
Chloe && James <ChloeAndJames@github.com>
2015-08-03 12:04:05 -0400

    Add #board_spaces helper method for Board Class.

--- minesweeper.rb
+++ minesweeper.rb
@@ -2,5 +2,25 @@ class Board
   attr_reader :grid
   def initialize
     @grid = Array.new(9) { Array.new(9) }
+    place_bombs
   end
+
+
+  def board_spaces
+    spaces = []
+    (0..8).each do |row|
+      (0..8).each do |space|
+        spaces << [row, space]
+      end
+    end
+    spaces
+  end
+
+  def place_bombs
+    # bomb_spaces = board_spaces.sample(10)
+    # bomb_spaces.each do |space|
+    #
+    # end
+  end
+
 end
