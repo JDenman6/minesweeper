57812d8cbc484ea1beee88af16098dff6ade5835
Chloe && James <ChloeAndJames@github.com>
2015-08-03 13:50:48 -0400

    Add #play to Game class; add display to #play, stubbed out rest of #play.

--- game.rb
+++ game.rb
@@ -7,4 +7,16 @@ class Game
   def initialize
     @board = Board.new
   end
+
+  def play
+    board.display # display board to player
+
+    # get r/f & position (validate input)
+    # if flag, toggle display between "F" & "-"
+    # if reveal
+      # check if pos bomb
+        # bomb => game over
+      # not bomb => check surroundings for bombs & check pos valid?
+  end
+
 end
